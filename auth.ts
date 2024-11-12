9abbb99bef8c8775513a1d02e78892ad363e3727
Josh Auriemma <allocate@gmail.com>
2024-11-12 08:46:08 -0500

    Simplify login

--- auth.ts
+++ auth.ts
@@ -1,82 +1,27 @@
-import NextAuth from "next-auth"
-import { authConfig } from "./auth.config"
-import Credentials from "next-auth/providers/credentials"
-import { z } from "zod"
-import { prisma } from "@/lib/prisma"
-import bcrypt from "bcryptjs"
+import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/lib/prisma';
 
-async function getUser(email: string) {
-  try {
-    const user = await prisma.user.findUnique({
-      where: { email },
-      select: {
-        id: true,
-        email: true,
-        name: true,
-        password: true,
-      },
-    })
-    return user
-  } catch (error) {
-    console.error("Failed to fetch user:", error)
-    throw new Error("Failed to fetch user.")
-  }
-}
-
-export const { auth, signIn, signOut } = NextAuth({
-  ...authConfig,
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
-    Credentials({
-      async authorize(credentials) {
-        try {
-          const parsedCredentials = z
-            .object({ email: z.string().email(), password: z.string().min(6) })
-            .safeParse(credentials)
-
-          if (!parsedCredentials.success) {
-            console.error("Invalid credentials format")
-            return null
-          }
-
-          const { email, password } = parsedCredentials.data
-          const user = await getUser(email)
-          
-          if (!user || !user.password) {
-            console.error("User not found or password not set")
-            return null
-          }
-          
-          const passwordsMatch = await bcrypt.compare(password, user.password)
-
-          if (!passwordsMatch) {
-            console.error("Passwords do not match")
-            return null
-          }
-
-          return {
-            id: user.id,
-            email: user.email,
-            name: user.name,
-          }
-        } catch (error) {
-          console.error("Authorization error:", error)
-          return null
-        }
-      },
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id
-      }
-      return token
-    },
-    async session({ session, token }) {
+    session({ session, user }) {
       if (session.user) {
-        session.user.id = token.id as string
+        session.user.id = user.id;
       }
-      return session
-    }
-  }
-})
+      return session;
+    },
+  },
+});
