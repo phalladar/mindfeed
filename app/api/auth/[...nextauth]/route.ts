eb7cbb4e5d112dd225d22622099c19d6ed87592a
Josh Auriemma <allocate@gmail.com>
2024-11-15 13:36:17 -0500

    How about this

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,4 @@ import { GET, POST } from "@/lib/auth";
 
 export { GET, POST };
 
-// We need to export a config to prevent the middleware from running on these routes
-export const config = {
-  runtime: 'edge',
-};
+export const runtime = 'edge';
